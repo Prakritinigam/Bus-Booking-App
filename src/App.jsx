a3bcf009712ebe23233699a82c57ba79edd9812a
Prakriti Nigam <prakritinigam319@gmail.com>
2025-04-23 21:41:08 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Components/HomePage";
 import { BookingProvider } from "./BusContext/BookingContext";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify"; // Import ToastContainer
 import BusListing from "./Components/BusListing";
 import SelectBus from "./Components/SelectBus";
+import "react-toastify/dist/ReactToastify.css"; // Import the toast CSS
 
 function App() {
   return (
     <>
       <BookingProvider>
         <Navbar />
-
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/buses" element={<BusListing />} />
